a085ad795d63e7aca8b39b25575476fbe7a8c2c9
Andres-Mac <amovilla1103@gmail.com>
2020-01-31 17:33:32 -0500

    init routes

--- progxpaz/src/App.js
+++ progxpaz/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
 import {Home} from "./Components/Home";
+import {ProductPage} from "./Components/ProductPage";
+import {Route, Switch} from "react-router-dom";
+import {Category} from "./Components/Category";
 
 function App() {
   return (
-    <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/product">
+          <ProductPage />
+        </Route>
+        <Route path="/category">
+          <Category />
+        </Route>
+      </Switch>
   );
 }
 
