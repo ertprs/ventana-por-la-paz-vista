ae04802999c2a659e86104e0a67d63598c1d6fd7
Andres-Mac <amovillaw01@gmail.com>
2020-02-04 01:16:59 -0500

    added font awesome library, formatting

--- progxpaz/src/App.js
+++ progxpaz/src/App.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './Sass/index.scss';
+
 import { Home } from './Pages/Home';
 import { ProductPage } from './Pages/ProductPage';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Category } from './Components/Category';
-import Header from './Components/Header/Header';
+import { Header } from './Components/Header';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faSearch as search, faHeart as heartFill } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as heartStroke } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fab, search, heartFill, heartStroke);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header></Header>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/product'>
-          <ProductPage />
-        </Route>
-        <Route path='/category'>
-          <Category />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      <>
+        <Header />
+
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/product'>
+              <ProductPage />
+            </Route>
+            <Route path='/category'>
+              <Category />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+    </ >
   );
 }
 
