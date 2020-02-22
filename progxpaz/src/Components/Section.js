bb4e3d4a630cdd4ad78bf00a659646f57f76ef00
Andres-XL <amovilla1103@gmail.com>
2020-02-22 18:24:51 -0500

    unneeded, remove warning

--- progxpaz/src/Components/Section.js
+++ progxpaz/src/Components/Section.js
@@ -6,10 +6,10 @@ export class Section extends Component {
     state = {
         title : '',
         link : ''
-    }
+    };
 
     render() {
-        let {title, link} = this.state;
+        //let {title, link} = this.state;
 
         return (
             <div className={'section'}>
