bc09f0d0e76b506093c077819299c6a10ca4498c
Andres-XL <amovilla1103@gmail.com>
2020-02-22 18:34:03 -0500

    fix return

--- progxpaz/src/Pages/ProductPage.js
+++ progxpaz/src/Pages/ProductPage.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import ProductDescription from '../Components/ProductDescription';
 import Section from '../Components/Section';
+
+
 export class ProductPage extends Component {
   render() {
     return (
-      <>
+    <>
+        <ProductDescription />
         <div className='container'>
-          <div className='row'>
-            <div className='col-xs-12'>
-              <Section></Section>
+            <div className='row'>
+                <div className='col-xs-12'>
+                    <Section />
+                </div>
             </div>
-          </div>
         </div>
-      </>
+    </>
     );
-        <ProductDescription></ProductDescription>
   }
 }
