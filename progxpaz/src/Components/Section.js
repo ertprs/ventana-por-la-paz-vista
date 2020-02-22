20690ab6e108548726d41ad4c123f2a8f231e1a8
Andres-XL <amovilla1103@gmail.com>
2020-02-22 18:42:31 -0500

    adding section title

--- progxpaz/src/Components/Section.js
+++ progxpaz/src/Components/Section.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { ProductCard } from './ProductCard';
 
 export default class Section extends Component {
-  state = {
-    title: '',
-    link: '',
-  };
 
     state = {
         title : '',
@@ -13,37 +9,38 @@ export default class Section extends Component {
     };
 
     render() {
-        //let {title, link} = this.state;
-    return (
-      <div className={'section'}>
-        <div className={'title'}>
-          <span className={'main'}> Lo Mas Vendido </span>
-          <span className={'link'}> ver mas </span>
-        </div>
-        <div className={'flex-row'}>
-          <ProductCard
-            name='Blusa'
-            price={30000}
-            category='Ropa'
-            discountPrice={15000}
-            discountPercentage={50}
-          />
-          <ProductCard
-            name='Zapatos'
-            price={60000}
-            category='Ropa'
-            discountPrice={54000}
-            discountPercentage={10}
-          />
-          <ProductCard
-            name='Correa'
-            price={20000}
-            category='Ropa'
-            discountPrice={15000}
-            discountPercentage={20}
-          />
-        </div>
-      </div>
-    );
+        let {title} = this.state;
+
+        return (
+          <div className={'section'}>
+            <div className={'title'}>
+              <span className={'main'}> {title} </span>
+              <span className={'link'}> ver mas </span>
+            </div>
+            <div className={'flex-row'}>
+              <ProductCard
+                name='Blusa'
+                price={30000}
+                category='Ropa'
+                discountPrice={15000}
+                discountPercentage={50}
+              />
+              <ProductCard
+                name='Zapatos'
+                price={60000}
+                category='Ropa'
+                discountPrice={54000}
+                discountPercentage={10}
+              />
+              <ProductCard
+                name='Correa'
+                price={20000}
+                category='Ropa'
+                discountPrice={15000}
+                discountPercentage={20}
+              />
+            </div>
+          </div>
+        );
   }
 }
