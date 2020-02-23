99da67a2583e8da35a451550dd8ffd8141e9acee
Andres-XL <amovilla1103@gmail.com>
2020-02-22 19:01:16 -0500

    fix input error

--- progxpaz/src/Components/Search.js
+++ progxpaz/src/Components/Search.js
@@ -6,7 +6,7 @@ export class Search extends Component {
     render() {
         return (
             <div className={'search-bar'}>
-                <Input placeholder='¿ Qué estás buscando hoy ?' className={'input'} id='searchbar'> </Input>
+                <Input placeholder='¿ Qué estás buscando hoy ?' className={'input'} id='searchbar'/>
 
                 <select className={'custom-select type-select'} name='filter'>
                     <option value='tipo'>tipo</option>
