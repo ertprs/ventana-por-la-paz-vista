71a24d955214bcab4f44e90c5936981655cc61e1
Yennifer Herrera <yenniferh@uninorte.edu.co>
2020-07-02 21:02:20 -0500

    ShopPage: Modal edit form

--- progxpaz/src/Pages/ShopPage.js
+++ progxpaz/src/Pages/ShopPage.js
@@ -15,6 +15,8 @@ import Section from '../Components/Section';
 
 export function ShopPage() {
   const [modal, setModal] = useState(false);
+  const [title, setTitle] = useState(false);
+  const [description, setDescription] = useState(false);
 
   const toggle = () => {
     setModal(!modal);
@@ -29,14 +31,13 @@ export function ShopPage() {
           </div>
           <div className='col-md-6 col-sm-12 align-self-start mt-3'>
             <div className='d-flex align-items-baseline'>
-              <h1 className='mr-3'>Nombre de Tienda</h1>
+              <h1 className='mr-3'>{title ? title : 'Nombre de Tienda'}</h1>
               <LikeButton />
             </div>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
-              voluptate, sint quo beatae perferendis ipsum fugiat sapiente velit
-              aut? Magni dolor, placeat fugit dignissimos totam deleniti.
-              Mollitia omnis ab perferendis?
+              {description
+                ? description
+                : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptate, sint quo beatae perferendis ipsum fugiat sapiente velit aut? Magni dolor, placeat fugit dignissimos totam deleniti. Mollitia omnis ab perferendis'}
             </p>
 
             <div className='tags-container'>
@@ -63,6 +64,7 @@ export function ShopPage() {
                       name='title'
                       id='title'
                       placeholder='Tienda la prosperidad'
+                      onChange={(e) => setTitle(e.target.value)}
                     />
                   </FormGroup>
                   <FormGroup>
@@ -72,16 +74,17 @@ export function ShopPage() {
                       name='description'
                       id='description'
                       placeholder='Venta de calzado'
+                      onChange={(e) => setDescription(e.target.value)}
                     />
                   </FormGroup>
                 </Form>
               </ModalBody>
               <ModalFooter>
                 <Button color='secondary' onClick={toggle} className='mr-2'>
-                  Cancel
+                  Cancelar
                 </Button>
                 <Button color='primary' onClick={toggle}>
-                  Do Something
+                  Editar
                 </Button>
               </ModalFooter>
             </Modal>
