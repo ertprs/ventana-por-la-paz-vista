a445e029f0e9ffb77ab16651ee4fcb6aae732a86
Andres-XL <amovilla1103@gmail.com>
2020-08-17 20:37:41 -0500

    implement hooks for likebutton

--- progxpaz/src/Components/Objects/LikeButton.js
+++ progxpaz/src/Components/Objects/LikeButton.js
@@ -1,38 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export class LikeButton extends Component {
-  state = {
-    taken: false,
-  };
-
-  setLiked = () => {
-    this.setState({
-      taken: !this.state.taken,
-    });
-  };
-
-  render() {
-    let { taken } = this.state;
+export function LikeButton() {
+    const [ isLiked, setLiked ] = useState(false);
+    
     return (
-      <div
-        className={
-          ' h-10 w-10 p-1 mr-2 mt-2 z-10 rounded-full bg-secondary-100 shadow-md flex justify-center items-center duration-200 '
-        }
-        onClick={this.setLiked}
-      >
-        {taken ? (
-          <FontAwesomeIcon
-            className={'h-12 w-12 text-primary-500 duration-200 scale-150'}
-            icon={'heart'}
-          />
-        ) : (
-          <FontAwesomeIcon
-            className={'h-12 w-12 text-primary-400'}
-            icon={['far', 'heart']}
-          />
-        )}
-      </div>
+        <div
+            className={
+                'h-10 w-10 p-1 mr-2 mt-2 z-10 rounded-full bg-secondary-100 shadow-md flex justify-center items-center duration-200'
+            }
+            onClick={() => setLiked(!isLiked)}
+        >
+            {isLiked ? (
+                <FontAwesomeIcon
+                    className={'h-12 w-12 text-primary-500 duration-200 scale-150'}
+                    icon={'heart'}
+                />
+            ) : (
+                <FontAwesomeIcon
+                    className={'h-12 w-12 text-primary-400'}
+                    icon={[ 'far', 'heart' ]}
+                />
+            )}
+        </div>
     );
-  }
+    
 }
