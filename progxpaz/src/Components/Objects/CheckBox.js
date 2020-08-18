d7167e49c1bd7efebae8795ffbd694a756cb82c4
Andres-XL <amovilla1103@gmail.com>
2020-08-17 20:42:22 -0500

    implement hooks for checkbox

--- progxpaz/src/Components/Objects/CheckBox.js
+++ progxpaz/src/Components/Objects/CheckBox.js
@@ -1,46 +1,17 @@
-import React, {Component} from 'react';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export class CheckBox extends Component {
-
-
-    constructor(props, context) {
-        super(props,context);
-
-        this.state = {
-            desc : props.text,
-            pressed : props.pressed
-        };
-        //this.updateDesc(props.text, props.pressed);
-    }
-
-    updateDesc(newDesc, newState = false) {
-        if (this.state.desc !== newDesc || this.state.pressed !== newState) {
-            this.setState({
-                desc : newDesc,
-                pressed : newState
-            })
-        }
-    }
-
-    setChecked = () => {
-        this.setState({
-            pressed : !this.state.pressed
-        })
-    };
-
-    render() {
-        let {desc, pressed} = this.state;
-
-        return (
-            <div className={'check-box'} onClick={this.setChecked}>
-                {pressed ?
-                    <FontAwesomeIcon className={'icon pressed'} icon={'check-square'}/>
-                    :
-                    <FontAwesomeIcon className={'icon'} icon={'square'}/>
-                }
-                <span className={'text'}>{desc}</span>
-            </div>
-        );
-    }
+export function CheckBox( { text } ) {
+    const [ isPressed, setPressed ] = useState(false);
+    
+    return (
+        <div className={'check-box'} onClick={() => setPressed(!isPressed)}>
+            <FontAwesomeIcon
+                className={'icon'+(isPressed? ' pressed':'')}
+                icon={(isPressed?'check-':'')+'square'}
+            />
+            <span className={'text'}>{text}</span>
+        </div>
+    );
 }
+
