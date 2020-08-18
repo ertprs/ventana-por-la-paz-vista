43114e43ab7cfcb9ccc2b448bc5d662b92b4124d
Andres-XL <amovilla1103@gmail.com>
2020-08-17 20:33:17 -0500

    implement hooks for input field

--- progxpaz/src/Components/Objects/InputField.js
+++ progxpaz/src/Components/Objects/InputField.js
@@ -1,56 +1,24 @@
-import React, { Component } from 'react';
-import { Input } from 'reactstrap';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export class InputField extends Component {
-  state = {
-    isPassword: false,
-    visible: false,
-  };
-
-  componentDidMount() {
-    this.setState({
-      isPassword: this.props.password,
-    });
-  }
-
-  flipVisible = () => {
-    this.setState({
-      visible: !this.state.visible,
-    });
-  };
-
-  setInputFocus = () => {
-    this.trueInput.focus();
-  };
-
-  render() {
-    let { isPassword, visible } = this.state;
-
+export function InputField( { password, id, name, value, onChange } ) {
+    const [ isVisible, setVisible ] = useState(false)
+    
     return (
-        <div style={{display: 'flex', alignItems: 'center'}}>
-      <input
-          ref={(input) => {
-            this.trueInput = input;
-          }}
-          id={this.props.id}
-          name={this.props.name}
-          value={this.props.value}
-          onChange={this.props.onChange}
-          type={isPassword && !visible ? 'password' : 'text'}
-      />
-        {isPassword ? (
-          <div className={'icon'} onClick={this.flipVisible}>
-            {visible ? (
-              <FontAwesomeIcon className={'icon'} icon={'eye'} />
-            ) : (
-              <FontAwesomeIcon className={'icon'} icon={'eye-slash'} />
-            )}
-          </div>
-        ) : (
-          <> </>
-        )}
-      </div>
+        <div style={{ display : 'flex', alignItems : 'center' }}>
+            <input
+                id={id}
+                name={name}
+                value={value}
+                onChange={onChange}
+                type={password && !isVisible ? 'password' : 'text'}
+            />
+            
+            {password ? (
+                <div className={'icon'} onClick={() => setVisible(!isVisible)}>
+                    <FontAwesomeIcon className={'icon'} icon={'eye'+(!isVisible?'-slash':'')} />
+                </div>
+            ) : <> </>}
+        </div>
     );
-  }
 }
