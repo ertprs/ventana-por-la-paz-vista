ef9bc12523d4c329966de7a1ffcae517597d9a37
Yennifer Herrera <yenniferh@uninorte.edu.co>
2020-09-04 00:25:01 -0500

    ShopPage design

--- progxpaz/src/Pages/ShopPage.js
+++ progxpaz/src/Pages/ShopPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { LikeButton } from '../Components/Objects';
 import { Section } from '../Components/Section';
 import ProductCard from '../Components/ProductCard';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Button,
   Modal,
@@ -26,63 +27,76 @@ export function ShopPage() {
   return (
     <>
       <div className='container'>
-        <div className='flex '>
-          <div className='w-2/5' />
-          <div className='w-full h-full rounded-full bg-primary-400'>H</div>
-        </div>
-        <div className='w.3/5'>
-          <div className={'d-flex align-items-baseline'}>
-            <h1 className={'mr-3'}>{title ? title : 'Nombre de Tienda'}</h1>
-            <LikeButton />
+        <div className='flex mt-10'>
+          <div className=''>
+            <div className=' w-32 h-32 rounded-full bg-primary-400 m-8' />
           </div>
-          <p>
-            {description
-              ? description
-              : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptate, sint quo beatae perferendis ipsum fugiat sapiente velit aut? Magni dolor, placeat fugit dignissimos totam deleniti. Mollitia omnis ab perferendis'}
-          </p>
+          <div className='w-3/5 ml-4'>
+            <div className={'flex items-center mb-8'}>
+              <h1
+                className={
+                  'mr-3 text-xl md:text-3xl text-secondary-500 font-semibold'
+                }
+              >
+                {title ? title : 'Nombre de Tienda'}
+              </h1>
+              <LikeButton />
+            </div>
+            <p>
+              {description
+                ? description
+                : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere voluptate, sint quo beatae perferendis ipsum fugiat sapiente velit aut? Magni dolor, placeat fugit dignissimos totam deleniti. Mollitia omnis ab perferendis'}
+            </p>
 
-          <button className={'mt-3 btn btn-primary'} onClick={toggle}>
-            Editar
-          </button>
-          <Modal
-            isOpen={modal}
-            toggle={toggle}
-            className={'modal-dialog-centered'}
-          >
-            <ModalHeader toggle={toggle}>Editar tienda</ModalHeader>
-            <ModalBody>
-              <Form>
-                <FormGroup>
-                  <Label for='title'>Nombre de la tienda</Label>
-                  <Input
-                    type='text'
-                    name='title'
-                    id='title'
-                    placeholder='Tienda la prosperidad'
-                    onChange={(e) => setTitle(e.target.value)}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label for='description'>Descripción de la tienda</Label>
-                  <Input
-                    type='textarea'
-                    name='description'
-                    id='description'
-                    placeholder='Venta de calzado'
-                    onChange={(e) => setDescription(e.target.value)}
-                  />
-                </FormGroup>
-              </Form>
-            </ModalBody>
-            <ModalFooter>
-              <Button color='secondary' onClick={toggle} className={'mr-2'}>
-                Cancelar
-              </Button>
-              <Button color='primary' onClick={toggle}>
-                Editar
-              </Button>
-            </ModalFooter>
-          </Modal>
+            <button
+              className={
+                'p-2 my-2 block rounded-md border border-primary-500 text-primary-500 text-center'
+              }
+              onClick={toggle}
+            >
+              Editar
+              <FontAwesomeIcon className={'ml-2 text-xl'} icon='pen' />
+            </button>
+            <Modal
+              isOpen={modal}
+              toggle={toggle}
+              className={'modal-dialog-centered'}
+            >
+              <ModalHeader toggle={toggle}>Editar tienda</ModalHeader>
+              <ModalBody>
+                <Form>
+                  <FormGroup>
+                    <Label for='title'>Nombre de la tienda</Label>
+                    <Input
+                      type='text'
+                      name='title'
+                      id='title'
+                      placeholder='Tienda la prosperidad'
+                      onChange={(e) => setTitle(e.target.value)}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <Label for='description'>Descripción de la tienda</Label>
+                    <Input
+                      type='textarea'
+                      name='description'
+                      id='description'
+                      placeholder='Venta de calzado'
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </FormGroup>
+                </Form>
+              </ModalBody>
+              <ModalFooter>
+                <Button color='secondary' onClick={toggle} className={'mr-2'}>
+                  Cancelar
+                </Button>
+                <Button color='primary' onClick={toggle}>
+                  Editar
+                </Button>
+              </ModalFooter>
+            </Modal>
+          </div>
         </div>
       </div>
       <Section title={'Productos'}>
