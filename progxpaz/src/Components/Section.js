cffaf5259c5326bb6a4807f1c50568d602e5f2ac
Andres-XL <amovilla1103@gmail.com>
2020-10-25 00:32:54 -0500

    removed fade from carousels

--- progxpaz/src/Components/Section.js
+++ progxpaz/src/Components/Section.js
@@ -99,7 +99,7 @@ export function ImageCarrousel( { children } ) {
         } else if ( endResult >= maxScroll ) {
             endResult = 0;
         }
-        console.log(endResult)
+        // console.log(endResult)
         setScroll(endResult);
     }
     
