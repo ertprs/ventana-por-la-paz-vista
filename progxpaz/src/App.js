2afb3f67f456e4ec1b5c8c82bac68c18a4da54c3
Andres-XL <amovilla1103@gmail.com>
2020-10-31 21:42:26 -0500

    app is component

--- progxpaz/src/App.js
+++ progxpaz/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Sass/app.scss';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
-import Home from './Pages/Home';
+import { Home } from './Pages/Home';
 import { ProductPage } from './Pages/ProductPage';
 import { ShopPage } from './Pages/ShopPage';
 import Header from './Components/Header';
 import { Provider } from './AuthContext';
 
-export default function App() {
-    return (
-        <Provider>
-            <BrowserRouter>
-                <Header />
-                <div className={'main-content'}>
-                    <Switch>
-                        <Route exact path='/' component={Home} />
-                        <Route path='/product' component={ProductPage} />
-                        <Route path='/shop' component={ShopPage} />
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        </Provider>
-    );
+export class App extends Component {
+    
+    render() {
+        return (
+            <Provider>
+                <BrowserRouter>
+                    <Header />
+                    <div className={'main-content'}>
+                        <Switch>
+                            <Route exact path='/' component={Home} />
+                            <Route path='/product' component={ProductPage} />
+                            <Route path='/shop' component={ShopPage} />
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
