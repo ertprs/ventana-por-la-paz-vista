f4dee84c5ffe998e263c28fffc19fa999498692f
Andres-XL <amovilla1103@gmail.com>
2020-10-31 21:42:40 -0500

    home fetches shops

--- progxpaz/src/Pages/Home.js
+++ progxpaz/src/Pages/Home.js
@@ -1,70 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Carrousel, ImageCarrousel } from '../Components/Section';
 import ProductCard from '../Components/ProductCard';
 import { ImageCard } from "../Components/ImageCard";
+import { SearchShops } from "../Services/Api";
 
-export default function Home() {
-  return (
-    <>
-      <ImageCarrousel>
-        <ImageCard
-            alt={'hi'}
-            link={'/product'}
-            curimg={1}
-          />
-          <ImageCard
-              alt={'hi'}
-              link={'/product'}
-              curimg={2}
-          />
-      </ImageCarrousel>
-      <Carrousel title={'Las Ventanas Más Vistas'}>
-        <ProductCard
-          name='Almacén Rosalía'
-          price={30000}
-          category='Ropa'
-          discountPrice={15000}
-          discountPercentage={50}
-          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem laudantium unde reiciendis tenetur minus harum tempore accusamus perspiciatis voluptas ex est atque autem ab illo eligendi, placeat repudiandae alias!'
-          shop={true}
-        />
-        <ProductCard
-          name='Exportaciones All-Sports'
-          price={60000}
-          category='Ropa'
-          discountPrice={54000}
-          discountPercentage={10}
-          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem laudantium unde reiciendis tenetur minus harum tempore accusamus perspiciatis voluptas ex est atque autem ab illo eligendi, placeat repudiandae alias!'
-          shop={true}
-        />
-        <ProductCard
-          name='Artesanías la esperanza '
-          price={20000}
-          category='Ropa'
-          discountPrice={15000}
-          discountPercentage={20}
-          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem laudantium unde reiciendis tenetur minus harum tempore accusamus perspiciatis voluptas ex est atque autem ab illo eligendi, placeat repudiandae alias!'
-          shop={true}
-        />
-        <SampleProduct />
-        <SampleProduct />
-        <SampleProduct />
-        <SampleProduct />
-      </Carrousel>
-    </>
-  );
-}
-
-function SampleProduct() {
-  return (
-      <ProductCard
-          name='Artesanías la esperanza '
-          price={20000}
-          category='Ropa'
-          discountPrice={15000}
-          discountPercentage={20}
-          description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat quidem laudantium unde reiciendis tenetur minus harum tempore accusamus perspiciatis voluptas ex est atque autem ab illo eligendi, placeat repudiandae alias!'
-          shop={true}
-      />
-  )
+export class Home extends Component {
+    
+    state = {
+        shopList : []
+    }
+    
+    componentDidMount() {
+        SearchShops().then(r => {
+            this.setState({
+                shopList : r
+            })
+        })
+    }
+    
+    render() {
+        const { shopList } = this.state;
+        
+        return (
+            <>
+                <ImageCarrousel>
+                    <ImageCard
+                        alt={'hi'}
+                        link={'/product'}
+                        curimg={1}
+                    />
+                    <ImageCard
+                        alt={'hi'}
+                        link={'/product'}
+                        curimg={2}
+                    />
+                </ImageCarrousel>
+                <Carrousel title={'Las Ventanas Más Vistas'}>
+                    {
+                        shopList.map(( item, i ) => {
+                            return <ProductCard
+                                description={item.descripcion}
+                                shop={true}
+                                name={item.nombre}
+                                key={i}
+                            />
+                        })
+                    }
+                </Carrousel>
+            </>
+        );
+    }
 }
--- progxpaz/src/Services/Api.js
+++ progxpaz/src/Services/Api.js
@@ -1,73 +1,76 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: `${process.env.REACT_APP_APIURL}`,
-  timeout: 5000,
-  responseType: 'json',
-  headers: { 'Content-Type': 'application/json' },
+    baseURL : `${process.env.REACT_APP_APIURL}`,
+    timeout : 5000,
+    responseType : 'json',
+    headers : { 'Content-Type' : 'application/json' },
 });
 
 let TOKEN;
 
-export async function Login(username, password) {
-  const data = { username: username, password: password };
-
-  return await API.post('/auth/login/', JSON.stringify(data))
-    .then((res) => {
-      TOKEN = res.data['key'];
-      API.defaults.headers.common['Authorization'] = `Token ${TOKEN}`;
-      return res;
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function Login( username, password ) {
+    const data = { username : username, password : password };
+    
+    return await API.post('/auth/login/', JSON.stringify(data))
+        .then(( res ) => {
+            TOKEN = res.data['key'];
+            API.defaults.headers.common['Authorization'] = `Token ${TOKEN}`;
+            return res;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 export async function Logout() {
-  return await API.post('auth/logout/')
-    .then((res) => res)
-    .catch((err) => {
-      console.error(`Error: ${err}`);
+    return await API.post('auth/logout/', {
+        headers : {
+            Authorization : `Token ${process.env.REACT_APP_TOKEN}`,
+        },
+    }).then(( res ) => res
+    ).catch(( err ) => {
+        console.error(`Error: ${err}`);
     });
 }
 
-export async function CreateProfile(nombre, correo, contraseña) {
-  const data = {
-    nombre,
-    correo,
-    contraseña,
-    edad: 18,
-  };
-  return await API.post('/api/v1/tps/perfiles/', JSON.stringify(data), {
-    headers: {
-      Authorization: `Token ${process.env.REACT_APP_TOKEN}`,
-    },
-  })
-    .then((res) => res)
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function CreateProfile( nombre, correo, contraseña ) {
+    const data = {
+        nombre,
+        correo,
+        contraseña,
+        edad : 18,
+    };
+    return await API.post('/api/v1/tps/perfiles/', JSON.stringify(data), {
+        headers : {
+            Authorization : `Token ${process.env.REACT_APP_TOKEN}`,
+        },
+    })
+        .then(( res ) => res)
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 export async function CreateShop(
-  nombre, descripcion, indicativo, whatsapp, ubicacion
+    nombre, descripcion, indicativo, whatsapp, ubicacion
 ) {
-  const data = {
-    nombre: nombre,
-    descripcion: descripcion,
-    indicativo: parseInt(indicativo),
-    whatsapp: parseInt(whatsapp),
-    ubicacion: ubicacion,
-  };
-  return await API.post('/api/v1/tps/tiendas/', JSON.stringify(data), {
-    headers: {
-      Authorization: `Token ${process.env.REACT_APP_TOKEN}`,
-    },
-  })
-    .then((res) => res)
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+    const data = {
+        nombre : nombre,
+        descripcion : descripcion,
+        indicativo : parseInt(indicativo),
+        whatsapp : parseInt(whatsapp),
+        ubicacion : ubicacion,
+    };
+    return await API.post('/api/v1/tps/tiendas/', JSON.stringify(data), {
+        headers : {
+            Authorization : `Token ${process.env.REACT_APP_TOKEN}`,
+        },
+    })
+        .then(( res ) => res)
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // export async function LinkProfileShop( idProfile, idShop ) {
@@ -89,112 +92,130 @@ export async function CreateShop(
 //         .then(( res ) => res)
 //         .catch(( err ) => console.log(`Error: ${err}`));
 // }
-export async function LinkProfileShop(idProfile, idShop) {
-  const data = {
-    tienda: idShop,
-  };
-
-  return await API.put(
-    `/api/v1/tps/perfiles/${idProfile}/`,
-    JSON.stringify(data),
-    {
-      headers: {
-        Authorization: `Token ${process.env.REACT_APP_TOKEN}`,
-      },
-    }
-  )
-    .then((res) => res)
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function LinkProfileShop( idProfile, idShop ) {
+    const data = {
+        tienda : idShop,
+    };
+    
+    return await API.put(
+        `/api/v1/tps/perfiles/${idProfile}/`,
+        JSON.stringify(data),
+        {
+            headers : {
+                Authorization : `Token ${process.env.REACT_APP_TOKEN}`,
+            },
+        }
+    )
+        .then(( res ) => res)
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // info de tienda [GET]
 // /api/v1/tiendas/id
-export async function ShopInfo(shopid) {
-  return await API.get('/api/v1/tps/tiendas/' + shopid)
-    .then((r) => {
-      return r;
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function ShopInfo( shopid ) {
+    return await API.get('/api/v1/tps/tiendas/' + shopid)
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // tiendas ordenadas por visitas [GET]
 // /api/v1/tiendas
 // ?param=visitas
 export async function PopularShops() {
-  return await API.get('/api/v1/tps/tiendas/', {
-    params: {
-      param: 'visitas',
-    },
-  })
-    .then((r) => {
-      return r;
+    return await API.get('/api/v1/tps/tiendas/', {
+        params : {
+            param : 'visitas',
+        },
     })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // tiendas ordenadas por fecha de creacion [GET]
 // /api/v1/tiendas
 // ?param=recientes
 export async function RecentShops() {
-  return await API.get('/api/v1/tps/tiendas/', {
-    params: {
-      param: 'recientes',
-    },
-  })
-    .then((r) => {
-      return r;
+    return await API.get('/api/v1/tps/tiendas/', {
+        params : {
+            param : 'recientes',
+        },
     })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // visitar tienda [GET]
 // /api/v1/tiendas/id/visit
-export async function VisitShop(shopid) {
-  return await API.get('/api/v1/tps/tiendas/' + shopid + '/visit')
-    .then((r) => {
-      return r;
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function VisitShop( shopid ) {
+    return await API.get('/api/v1/tps/tiendas/' + shopid + '/visit')
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // busqueda [GET]
 // /ventanapaz/api/v1/tps/tiendas/filter_tienda/
 // ?words=busqueda
-export async function SearchShops(shopid, search) {
-  return await API.get('/api/v1/tps/tiendas/filter_tienda', {
-    params: {
-      words: search,
-    },
-  })
-    .then((r) => {
-      return r;
+export async function SearchShops( search, page ) {
+    return await API.get('/api/v1/tps/tiendas/', {
+        params : {
+            words : search,
+            page : page
+        },
+        headers : {
+            Authorization : `Token ${process.env.REACT_APP_TOKEN}`,
+        },
     })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+        .then(( r ) => {
+            return r.data.results;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
+}
+
+export async function SearchProducts( shopid, search ) {
+    return await API.get('/api/v1/tps/tiendas/filter_by_product', {
+        params : {
+            words : search,
+        },
+    })
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
 
 // cambiar contraseña [POST]
 // /ventanapaz/api/v1/tps/tiendas/filter_tienda/
 // body: new_password1,new_password2
 // header: usuario autenticado
-export async function ChangePassword(new_password1, new_password2) {
-  const data = { new_password1, new_password2 };
-  return await API.post('/api/v1/tps/tiendas/filter_tienda', data)
-    .then((r) => {
-      return r;
-    })
-    .catch((err) => {
-      console.log(`Error: ${err}`);
-    });
+export async function ChangePassword( new_password1, new_password2 ) {
+    const data = { new_password1, new_password2 };
+    return await API.post('/api/v1/tps/tiendas/filter_tienda', data)
+        .then(( r ) => {
+            return r;
+        })
+        .catch(( err ) => {
+            console.log(`Error: ${err}`);
+        });
 }
