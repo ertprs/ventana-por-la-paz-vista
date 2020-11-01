b1286e9f0207d27879e4429c23235871bb2bb890
Andres-XL <amovilla1103@gmail.com>
2020-10-31 22:07:45 -0500

    fix search with no params

--- progxpaz/src/Services/Api.js
+++ progxpaz/src/Services/Api.js
@@ -173,7 +173,7 @@ export async function VisitShop( shopid ) {
 // busqueda [GET]
 // /ventanapaz/api/v1/tps/tiendas/filter_tienda/
 // ?words=busqueda
-export async function SearchShops( { search, page = 1 } ) {
+export async function SearchShops( { search, page = 1 }={} ) {
     return await API.get('/api/v1/tps/tiendas/', {
         params : {
             words : search,
