7a15eda6a9a1b3bc02200910d003069ecaccc879
Andres-XL <amovilla1103@gmail.com>
2021-01-24 19:46:56 -0500

    revise footer

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ export class App extends Component {
                         />
                     </Switch>
                 </BrowserRouter>
-                <Footer/>
+                <Footer />
             </Provider>
         );
     }
@@ -76,7 +76,32 @@ export class App extends Component {
 
 
 const Footer = () => {
-    return <footer className='w-full mt-3 p-4 text-gray-600 bg-primary-100'>
+    return <footer className='w-full mt-3 p-4 text-gray-600 bg-primary-100 flex flex-col items-center'>
+        
+        <div className='w-3/4 flex flex-row justify-evenly'>
+            <div className="m-3 flex-1">
+                <img src={LogoPeace} className=' h-20' alt='Logo de PeaceDev' />
+            </div>
+            <div className="m-3 flex-1">
+                <img src={LogoArn} className='w-40' alt='Logo de ARN' />
+            </div>
+            <div className="m-3 flex-1">
+                <div className="flex justify-evenly">
+                    <ul>
+                        <li>Yennifer Herrera</li>
+                        <li>Jaisir Bayuelo</li>
+                    </ul>
+                    <ul>
+                        <li>Andres Movilla</li>
+                        <li>Carlos Ruiz</li>
+                    </ul>
+                </div>
+                <div className={'flex justify-center'}>
+                    Angie Zuñiga
+                </div>
+            </div>
+        </div>
+        
         <p className='text-center'>
             Hecho con
             <span>
@@ -86,35 +111,20 @@ const Footer = () => {
                     icon={'heart'}
                 />
             </span>
-            por 
-            <a 
+            por
+            <a
                 href='https://www.peacedev.co'
                 className='text-gray-700'
                 target="_blank"
                 rel="noopener noreferrer"
             > PeaceDev</a>
             {' '} en alianza con {' '}
-            <a 
+            <a
                 href='http://www.reincorporacion.gov.co/'
                 className='text-gray-700'
                 target="_blank"
                 rel="noopener noreferrer"
             >ARN Atlántico</a>
         </p>
-        <div className='flex flex-row justify-center'>
-            <div className="mr-4">
-                <img src={LogoPeace} className=' h-20' alt='Logo de PeaceDev'/>
-                <img src={LogoArn} className='w-40' alt='Logo de ARN'/>
-            </div>
-            <div className="">
-                <ul>
-                    <li>Angie Zuñiga</li>
-                    <li>Yennifer Herrera</li>
-                    <li>Jaisir Bayuelo</li>
-                    <li>Andres Movilla</li>
-                    <li>Carlos Ruiz</li>
-                </ul>
-            </div>
-        </div>
     </footer>
 }
